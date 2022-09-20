46a0d23ec6cf97dbeeaab8aa5a98501af51c9ae8
Emmanuel Ochieng <eochieng16@gmail.com>
2022-09-20 10:52:53 +0300

    asdf

--- index.js
+++ index.js
@@ -1,20 +1,27 @@
-const express = require('express')
-const mongoose = require('mongoose')
-const session = require('express-session')
-const redis = require('redis')
-const cors = require('cors')
+const express = require("express");
+const mongoose = require("mongoose");
+const session = require("express-session");
+const redis = require("redis");
+const cors = require("cors");
 
-let RedisStore = require('connect-redis')(session)
+let RedisStore = require("connect-redis")(session);
 
-
-const {MONGO_USER, MONGO_PORT, MONGO_IP, MONGO_PASSWORD, REDIS_URL, SESSION_SECRET, REDIS_PORT} = require('./config/config')
+const {
+  MONGO_USER,
+  MONGO_PORT,
+  MONGO_IP,
+  MONGO_PASSWORD,
+  REDIS_URL,
+  SESSION_SECRET,
+  REDIS_PORT,
+} = require("./config/config");
 
 let redisClient = redis.createClient({
-    url: 'redis://redis:6379',
-    legacyMode: true
+  url: "redis://redis:6379",
+  legacyMode: true,
 });
 
-redisClient.connect()
+redisClient.connect();
 
 // const connectRedis = async () => {
 
@@ -23,48 +30,51 @@ redisClient.connect()
 
 // connectRedis()
 
-const postRouter = require('./routes/postRoutes')
-const userRouter = require('./routes/userRoutes')
+const postRouter = require("./routes/postRoutes");
+const userRouter = require("./routes/userRoutes");
 
+const app = express();
 
-const app = express()
-
-const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`
-
+const mongoURL = `mongodb://${MONGO_USER}:${MONGO_PASSWORD}@${MONGO_IP}:${MONGO_PORT}/?authSource=admin`;
 
 const connectWithRetry = () => {
-    mongoose.connect(mongoURL).then(()=> console.log("successfully connected to DB")).catch((e)=> {console.log(e)
-        setTimeout(connectWithRetry, 5000)}
-        )
-}
+  mongoose
+    .connect(mongoURL)
+    .then(() => console.log("successfully connected to DB"))
+    .catch((e) => {
+      console.log(e);
+      setTimeout(connectWithRetry, 5000);
+    });
+};
 
+connectWithRetry();
 
-connectWithRetry()
-
-app.enable("trust poxy")
-app.use(cors({}))
-app.use(session({
-    store: new RedisStore({client: redisClient}),
-    secret: SESSION_SECRET, 
+app.enable("trust poxy");
+app.use(cors({}));
+app.use(
+  session({
+    store: new RedisStore({ client: redisClient }),
+    secret: SESSION_SECRET,
     cookie: {
-        secure: false,
-        resave: false,
-        saveUninitialized: false,
-        httpOnly: true,
-        maxAge: 60000,
-    }
-}))
+      secure: false,
+      resave: false,
+      saveUninitialized: false,
+      httpOnly: true,
+      maxAge: 60000,
+    },
+  })
+);
 
-app.use(express.json())
+app.use(express.json());
 
-app.get('/api/v1', (req,res) => {
-    res.send('<h2>Hi There</h2>')
-    console.log("Running...")
-})
+app.get("/api/v1", (req, res) => {
+  res.send("<h2>Hi There!!!</h2>");
+  console.log("Running...");
+});
 
-app.use('/api/v1/posts', postRouter)
-app.use('/api/v1/users', userRouter)
+app.use("/api/v1/posts", postRouter);
+app.use("/api/v1/users", userRouter);
 
-const PORT =    process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`listening on PORT ${PORT}...`))
+app.listen(PORT, () => console.log(`listening on PORT ${PORT}...`));
